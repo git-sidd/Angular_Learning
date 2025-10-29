8ff63874d09a7579800e6b65cffc6d1de5f24995
git-sidd <siddpatole45@gmail.com>
2025-10-29 18:11:09 +0530

    reverting "Routing in Angular"
    This reverts commit 2f6ed08c585e2f641a8c83163e1b6ef9e95056fc.

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'headerElem', 
-  imports: [RouterLink,RouterLinkActive],
+  imports: [RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
