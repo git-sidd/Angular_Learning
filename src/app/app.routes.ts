9ee51fdd9176f11c928fd5c1475f3270477b59a1
git-sidd <siddpatole45@gmail.com>
2025-10-29 18:13:56 +0530

    Wild card Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { Counter } from './counter/counter';
 import { TodoApp } from './todo-app/todo-app';
 import { Login } from './login/login';
 import { Home } from './home/home';
+import { PageNotFound } from './page-not-found/page-not-found';
 
 export const routes: Routes = [
     {path:'counter',component:Counter},
     {path:'todo',component:TodoApp},
-    {path:'',component:Home}
+    {path:'',component:Home},
+    {path:'**',component:PageNotFound}
 ];
